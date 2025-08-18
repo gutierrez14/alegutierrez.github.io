168aeebec2d35a88aa5f0b9a208c787c22446af8
gutierrez14 <ale14@uab.edu>
2025-08-18 17:49:55 -0500

    added comments to .js and .html for deeper understanding

--- js/main.js
+++ js/main.js
@@ -1,40 +1,58 @@
-// get references to the element we need control
-const menuButton=document.getElementById('menuButton'); //hamburger button
+// G E T  R E F E R E N C E S  F R O M  P A G E
+
+// document : obj. that represents the entire web page (html doc)
+// .getElementById : searches thru the page to get elem. 
+const menuButton=document.getElementById('menuButton'); // get hamburger button by id="menuButton"
 const sidebar=document.getElementById('sidebar'); // slide-in panel <nav>
 const closeButton=document.getElementById('closeButton'); // close menu button "x"
 const backdrop=document.getElementById('backdrop'); //dark overlay behind the panel
-// open the sidebar: add classes and update ARIA attributes
 
+// F U N C T I O N S  T O  O P E N / C L O S E  S I D E B A R
 function openSidebar(){
-    sidebar.classList.add('open'); // CSS .backdrop.open => slide in
-    backdrop.classList.add('show'); // CSS .backdrop.show => show overlay
-    backdrop.hidden=false;
-    menuButton.setAttribute('aria-expanded','true');
-    sidebar.setAttribute('aria-hidden', 'false');
-    closeButton.focus();
+    sidebar.classList.add('open'); // adds a css class "open". CSS uses sidebar.open. to slide the sidebar into view. W/o "open" the sidebar stays hiddent.
+    backdrop.classList.add('show'); // makes the grey/black overlay appear
+    backdrop.hidden=false; //to be double safe, makes sure the backdrop is visible
+    menuButton.setAttribute('aria-expanded','true'); //updates accessibility, screen readers know the sidebar menu is open
+    sidebar.setAttribute('aria-hidden', 'false'); //tells assisstive tech that the sidebar is visible
+    closeButton.focus(); //moves keyboard focus to 'x'. Can interact with close button.
+
+    // focus : for keyboard users, 'tab' key moves from element to next available element. 
 }
 
 function closeSidebar(){
-    sidebar.classList.remove('open');
-    backdrop.classList.remove('show');
-    backdrop.hidden=true
-    menuButton.setAttribute('aria-expanded', 'false');
-    sidebar.setAttribute('aria-hidden', 'true');
-    menuButton.focus();
+    sidebar.classList.remove('open'); // removes "open" css class . CSS uses sidebar.close to close the sidebar out of view. W/o "open", the sidebar hides/slides out pf view.
+    backdrop.classList.remove('show'); // makes backdrop dissapear
+    backdrop.hidden=true //makes sure the backdrop is no longer in view 
+    menuButton.setAttribute('aria-expanded', 'false'); //updates status. tell screen readers the sidebar is not open (is collapsed) 
+    sidebar.setAttribute('aria-hidden', 'true'); // tells assistive tech that the sidebar is not visble/is hidden
+    menuButton.focus(); // moves keyboard focus back to hamburger button
 }
 
-//click handlers
+// W I R E  U P  I N T E R A C T I O N
 
+/*
+    menuButton.addEventListener : 
+        - listens for click on the hamburger/ menu button
+        - checks if sidebar has the CSS class "open"
+            > if open, call 'closeSidebar'
+            > if closed, call 'openSidebar'
+*/ 
 menuButton.addEventListener('click', ()=> {
     const isOpen=sidebar.classList.contains('open');
-    isOpen? closeSidebar() : openSidebar();
+    isOpen? closeSidebar() : openSidebar(); // if/else
 });
-closeButton.addEventListener('click', closeSidebar);
-backdrop.addEventListener('click', closeSidebar);
 
-//keyboard: Esc to close
-document.addEventListener('keydown', (e) => {
-    if(e.key == 'Escape' && sidebar.classList.contains('open')){
+closeButton.addEventListener('click', closeSidebar); //closes sidebar when 'x' is clicked
+backdrop.addEventListener('click', closeSidebar); //closes sidebar when backdrop is clicked
+
+
+/*
+    document.addEventListener : use 'ESC' to close
+        - listens for any key click
+        - if the key == ESC key && the side bar is open, call 'closeSidebar()'
+*/ 
+document.addEventListener('keydown', (event) => {
+    if(event.key == 'Escape' && sidebar.classList.contains('open')){
         closeSidebar();
     }
 });
